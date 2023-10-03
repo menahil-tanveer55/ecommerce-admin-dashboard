19e2f1ee9fbc37fcebba08acdc07b9d3d09f9a76
menahil-tanveer55 <menahal.tanveer@intagleo.com>
2023-10-04 02:54:52 +0500

    sales graph added

---
+++ EcommerceAdminDashboard/src/components/BarChart.vue
@@ -0,0 +1,74 @@
+<template>
+  <Bar id="sales-chart" :options="chartOptions" :data="chartData" />
+</template>
+
+<script lang="ts">
+import { Bar } from 'vue-chartjs'
+import { PropType } from 'vue'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+// Interfaces
+interface ISalesData {
+  data: number[]
+  labels: string[]
+}
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  props: {
+    salesData: {
+      type: Object as PropType<ISalesData>,
+      required: true
+    }
+  },
+  data() {
+    return {
+      selection: 2,
+      chartData: {
+        labels: [
+          'Jan',
+          'Feb',
+          'Mar',
+          'Apr',
+          'May',
+          'Jun',
+          'Jul',
+          'Aug',
+          'Sep',
+          'Oct',
+          'Nov',
+          'Dec'
+        ],
+        datasets: [
+          {
+            label: 'Sales 2022',
+            backgroundColor: '#E8EAF6',
+            borderColor: '#3F51B5',
+            borderWidth: 1,
+            data: [65, 59, 100, 120, 200, 300, 10, 0, 8, 11, 13, 130]
+          },
+          {
+            label: 'Sales 2023',
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+            data: [65, 70, 120, 50, 100, 310, 10, 5, 20, 30]
+          }
+        ]
+      },
+      chartOptions: {
+        responsive: true
+      }
+    }
+  }
+}
+</script>
--- EcommerceAdminDashboard/src/views/Overview.vue
+++ EcommerceAdminDashboard/src/views/Overview.vue
@@ -7,10 +7,26 @@
         :card-properties="data"
       />
     </div>
+    <div class="mt-10">
+      <v-card :elevation="0" class="pa-10 mx-6">
+        <div class="d-flex justify-space-between">
+          <p class="text-h6">Sales Trend</p>
+          <v-chip-group v-model="state.selection" column variant="outlined" color="indigo">
+            <v-chip label>Daily</v-chip>
+            <v-chip label>Weekly</v-chip>
+            <v-chip label>Monthly</v-chip>
+            <v-chip label>Annually</v-chip>
+          </v-chip-group>
+        </div>
+        <BarChart v-model="state.chartData"
+      /></v-card>
+    </div>
   </v-container>
 </template>
 <script setup lang="ts">
+import { reactive } from 'vue'
 import StatisticsCard from '@/components/StatisticsCard.vue'
+import BarChart from '@/Components/BarChart.vue'
 const statisticsData = [
   {
     title: 'total sales',
@@ -24,5 +40,12 @@ const statisticsData = [
     percentage: '2.4%'
   }
 ]
+const state = reactive({
+  selection: 2,
+  chartData: {
+    labels: [],
+    data: []
+  }
+})
 </script>
 <style scoped></style>
